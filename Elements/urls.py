28b2dec414fb571ab81d97664bdc453d248f5d36
Ink <inkstudio007@gmail.com>
2024-01-19 18:32:14 +0330

    d groups

--- Elements/urls.py
+++ Elements/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.periodic_table, name='periodic_table'),
-    # 1
     path('H/', views.hydrogen, name='H'),
     path('Li/', views.lithium, name='Li'),
     path('Na/', views.sodium, name='Na'),
@@ -11,7 +10,6 @@ urlpatterns = [
     path('Rb/', views.rubidium, name='Rb'),
     path('Cs/', views.cesium, name='Cs'),
     path('Fr/', views.francium, name='Fr'),
-
     # 2
     path('Be/', views.beryllium, name='Be'),
     path('Mg/', views.magnesium, name='Mg'),
@@ -20,4 +18,36 @@ urlpatterns = [
     path('Ba/', views.barium, name='Ba'),
     path('Ra/', views.radium, name='Ra'),
     # 3
-]
+    path('Sc/', views.scandium, name='Sc'),
+    path('Y/', views.yttrium, name='Y'),
+    # 4
+    path('Ti/', views.titanium, name='Ti'),
+    path('Zr/', views.zirconium, name='Zr'),
+    path('Hf/', views.hafnium, name='Hf'),
+    path('Rf/', views.rutherfordium, name='Rf'),
+    # 5
+    path('V/', views.vanadium, name='V'),
+    path('Nb/', views.nobium, name='Nb'),
+    path('Ta/', views.tantalum, name='Ta'),
+    path('Db/', views.dubnium, name='Db'),
+    # 6
+    path('Cr/', views.chromium, name='Cr'),
+    path('Mo/', views.molybdenum, name='Mo'),
+    path('W/', views.tungsten, name='W'),
+    path('Sg/', views.seaborgium, name='Sg'),
+    # 8
+    path('Fe/', views.iron, name='Fe'),
+    path('Ru/', views.ruthenium, name='Ru'),
+    path('Os/', views.osmium, name='Os'),
+    path('Hs/', views.hassium, name='Hs'),
+    # 10
+    path('Ni/', views.nickel, name='Ni'),
+    path('Pd/', views.palladium, name='Pd'),
+    path('Pt/', views.platinum, name='Pt'),
+    path('Ds/', views.darmstadtium, name='Ds'),
+    # 12
+    path('Zn/', views.zinc, name='Zn'),
+    path('Cd/', views.codmium, name='Cd'),
+    path('Hg/', views.mercury, name='Hg'),
+    path('Cn/', views.copernicium, name='Cn'),
+    ]
--- Elements/views.py
+++ Elements/views.py
@@ -65,3 +65,128 @@ def radium(request):
     return render(request, '2/Radium.html')
 
 
+# 3
+
+
+def scandium(request):
+    return render(request, '3/Scandium.html')
+
+
+def yttrium(request):
+    return render(request, '3/Yttrium.html')
+
+
+# 4
+
+
+def titanium(request):
+    return render(request, '4/Titanium.html')
+
+
+def zirconium(request):
+    return render(request, '4/Zirconium.html')
+
+
+def hafnium(request):
+    return render(request, '4/Hafnium.html')
+
+
+def rutherfordium(request):
+    return render(request, '4/Rutherfordium.html')
+
+
+# 5
+
+
+def vanadium(request):
+    return render(request, '5/Vanadium.html')
+
+
+def nobium(request):
+    return render(request, '5/Nobium.html')
+
+
+def tantalum(request):
+    return render(request, '5/Tantalum.html')
+
+
+def dubnium(request):
+    return render(request, '5/Dubnium.html')
+
+
+# 6
+
+
+def chromium(request):
+    return render(request, '6/Chromium.html')
+
+
+def molybdenum(request):
+    return render(request, '6/Molybdenum.html')
+
+
+def tungsten(request):
+    return render(request, '6/Tungsten.html')
+
+
+def seaborgium(request):
+    return render(request, '6/Seaborgium.html')
+
+
+# 8
+
+
+def iron(request):
+    return render(request, '8/Iron.html')
+
+
+def ruthenium(request):
+    return render(request, '8/Ruthenium.html')
+
+
+def osmium(request):
+    return render(request, '8/Osmium.html')
+
+
+def hassium(request):
+    return render(request, '8/Hassium.html')
+
+
+# 10
+
+
+def nickel(request):
+    return render(request, '10/Nickel.html')
+
+
+def palladium(request):
+    return render(request, '10/Palladium.html')
+
+
+def platinum(request):
+    return render(request, '10/Platinum.html')
+
+
+def darmstadtium(request):
+    return render(request, '10/Darmstadtium.html')
+
+
+# 12
+
+
+def zinc(request):
+    return render(request, '12/Zinc.html')
+
+
+def codmium(request):
+    return render(request, '12/Codmium.html')
+
+
+def mercury(request):
+    return render(request, '12/Mercury.html')
+
+
+def copernicium(request):
+    return render(request, '12/Copernicium.html')
+
+
